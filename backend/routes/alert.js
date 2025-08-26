e0703d9f4f47314f6e94e733c70530b37ff25b2e
orenb99 <77238204+orenb99@users.noreply.github.com>
2025-08-26 16:18:19 +0300

    Update alert.js

--- backend/routes/alert.js
+++ backend/routes/alert.js
@@ -2,7 +2,7 @@ const express = require("express");
 const prisma = require("../prisma/prisma-client");
 const { verifyToken } = require("../middleware/auth");
 const router = express.Router();
-
+const { AlertType } = require("@prisma/client");
 router.get("/top", verifyToken, async (req, res) => {
   try {
     const { inspectorId } = req.inspector;
@@ -13,6 +13,14 @@ router.get("/top", verifyToken, async (req, res) => {
     });
     const topFive = await prisma.alert.findMany({
       where: { inspectorId },
+      select: {
+        alertId: true,
+        title: true,
+        priority: true,
+        status: true,
+        dueDate: true,
+        alertType: true,
+      },
       take: 5,
       orderBy: [{ priority: "desc" }, { dueDate: "desc" }], // CHECK LATER THE SORTING KEY
     });
@@ -34,18 +42,14 @@ router.get("/all", verifyToken, async (req, res) => {
     // Fetch active alerts for the inspector
     const alerts = await prisma.alert.findMany({
       where: { inspectorId },
-      select: {
-        alertId: true,
-        title: true,
-        priority: true,
-        status: true,
-        dueDate: true,
-        alertType: true,
-      },
       orderBy: [{ priority: "desc" }, { dueDate: "desc" }],
     });
-
-    res.status(200).json({ message: "alerts fetched successfully", data: alerts });
+    // Return a list of alerts for each type
+    const data = {};
+    for (const type of Object.values(AlertType)) {
+      data[type] = alerts.filter((item) => item.alertType === type);
+    }
+    res.status(200).json({ message: "alerts fetched successfully", data });
   } catch (error) {
     console.error("Error fetching alerts:", error);
     res.status(500).json({ error: "Internal server error" });
